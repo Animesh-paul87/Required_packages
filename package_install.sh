0d3f260e5f1e0c898f5583cb5c313bcfdd4f53e4
Animesh Paul <animeshpaul.net@gmail.com>
2025-08-03 10:59:12 +0530

    Update package_install.sh
    adding output redirections

--- package_install.sh
+++ package_install.sh
@@ -3,15 +3,15 @@
 # if Clean command not working
 echo "For RHEL Based Systems" 
 echo "Update OS and upgrade OS"
-yum update && yum upgrade -y
+yum update 2&>/dev/null && yum upgrade -y 2&>/dev/null
 echo "Installing Clean Command"
-yum install ncurses -y
+yum install ncurses -y 2&>/dev/null
 # install for ifconfig
 echo "Installing net-tools"
-yum install net-tools -y
+yum install net-tools -y 2&>/dev/null
 # install for nano
 echo "Installing Nano Editor"
-yum install nano -y
+yum install nano -y 2&>/dev/null
 # install openssh-server
 echo "Installing Openssh-server"
-yum install openssh-server -y
+yum install openssh-server -y 2&>/dev/null
