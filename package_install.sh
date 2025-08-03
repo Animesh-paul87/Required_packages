f43c3d2a64807495394a968879f74022f82f4220
Animesh Paul <animeshpaul.net@gmail.com>
2025-08-03 11:21:08 +0530

    Update package_install.sh

--- package_install.sh
+++ package_install.sh
@@ -3,15 +3,15 @@
 # if Clean command not working
 echo "For RHEL Based Systems" 
 echo "Update OS and upgrade OS"
-yum update >/dev/null 2&>1 && yum upgrade -y >/dev/null 2&>1
+yum update > /dev/null 2>&1 && yum upgrade -y > /dev/null 2>&1
 echo "Installing Clean Command"
-yum install ncurses -y >/dev/null 2&>1
+yum install ncurses -y > /dev/null 2>&1
 # install for ifconfig
 echo "Installing net-tools"
-yum install net-tools -y >/dev/null 2&>1
+yum install net-tools -y > /dev/null 2>&1
 # install for nano
 echo "Installing Nano Editor"
-yum install nano -y >/dev/null 2&>1
+yum install nano -y > /dev/null 2>&1
 # install openssh-server
 echo "Installing Openssh-server"
-yum install openssh-server -y >/dev/null 2&>1
+yum install openssh-server -y > /dev/null 2>&1
