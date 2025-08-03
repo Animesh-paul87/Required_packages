fbb16a64d210415587ce9aca9267c48f1f356638
Animesh Paul <animeshpaul.net@gmail.com>
2025-08-03 10:49:27 +0530

    Create package_install.sh

---
+++ package_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# required for Lxc contontainer
+# if Clean command not working
+echo "Installing Clean Command"
+yum install ncurses -y
+# install for ifconfig
+echo "Installing net-tools"
+yum install net-tools -y
+# install for nano
+echo "Installing Nano Editor"
+yum install nano -y
+# install openssh-server
+echo "Installing Openssh-server"
+yum install openssh-server -y
